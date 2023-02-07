e86b616017efe64c6bca30b64261250072ed628d
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-07 17:29:56 +0300

    Task 28

--- js.js
+++ js.js
@@ -1,11 +1,8 @@
 let one = 'abcde';
-alert(one[0]); 
-alert(one[2]); 
-alert(one[4]); 
+alert(one[one.length - 1]);
 
 let two = 'abcde';
-alert(two[4]+two[3]+two[2]+two[1]+two[0]); 
+alert(two[two.length - 2]);
 
 let three = 'abcde';
-let num = 3; 
-alert(three[num]); 
+alert(three[three.length - 3]);
