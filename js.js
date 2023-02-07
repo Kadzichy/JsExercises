438ebe81a74bf1063b3d8679b462731c0a81177a
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-07 16:02:20 +0300

    Task 21

--- js.js
+++ js.js
@@ -1,2 +1,19 @@
-const a = 3.14;
-console.log(4 * a * 12);
+let a = '' + 3 + 1;
+alert(a);
+
+let b = 2 + 3 + '1';
+alert(b); 
+
+let c = '1' + 2 + 3;
+alert(c);
+
+let d = '1' + (2 + 3);
+alert(d);
+
+let e = 1 + '2';
+alert(e); 
+
+let f = '5' * '2';
+alert(f);
+
+alert('2' * '3s'); // выведет NaN
