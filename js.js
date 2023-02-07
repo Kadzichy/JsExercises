b7b8b9cab5c21569741933583cbbf5b315a42e36
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-07 15:42:34 +0300

    Task 19

--- js.js
+++ js.js
@@ -1,18 +1,4 @@
-let a;
-alert(a);
+let a = 'A';
+console.log(a);
 
-let b = null;
-alert(b);
-
-let c = true;
-alert(c);
-
-let d = false;
-alert(d);
-
-let e = 'abc, f = '2';
-alert(e * f);
-
-alert( 10 / 0);
-
-alert(-10 / 0); 
+alert(eee);
