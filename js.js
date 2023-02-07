49689493900572ef6fd36c3ba0d861b639b727ff
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-07 17:21:07 +0300

    Task 26

--- js.js
+++ js.js
@@ -1,7 +1,32 @@
-alert(true + 3);
-alert(true + true);
-alert(true - true);
-alert(true + false);
-alert(true + '1');
-alert(String(true) + 1);
-alert(String(true) + Number(true));
+let a = Boolean(3);
+alert(a);
+
+let b = Boolean(0);
+alert(b);
+
+let c = Boolean(-1);
+calert(c);
+
+let d = Boolean(-0);
+alert(d);
+
+let e = Boolean('alec');
+alert(e);
+
+let f = Boolean('');
+alert(f);
+
+let g = Boolean(3 * 'abs');
+alert(g);
+
+let h = Boolean('');
+alert(h);
+
+let i = Boolean('0');
+alert(j);
+
+let k = Boolean(true);
+alert(k);
+
+let l = Boolean(false);
+alert(l);
