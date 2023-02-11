9b7b379ffbe2682e63df8442cf97174ae483a79e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:26:57 +0300

    Task 33

--- js.js
+++ js.js
@@ -1,2 +1,12 @@
-alert(0.1 * 0.2);
-alert(0.3 * 0.1);
+alert('Your age: ' + prompt('Your age?')); 
+
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+alert(Number(num1) + Number(num2)); 
+
+let num3 = prompt('Сторона квадрата = ');
+alert(num3*num3);
+
+let num4 = prompt('Введите первую сторону прямоугольника');
+let num5 = prompt('Введите вторую сторону прямоугольника');
+alert(Number(num4) * Number(num5)); 
