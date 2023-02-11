06268f620b98b93a77b69265bb570cb5ae6120ec
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:34:34 +0300

    Task 36

--- js.js
+++ js.js
@@ -1,17 +1,5 @@
-let num1 = 1;
-let num2 = 2;
-alert('сумма: ', num1 + num2); 
+alert(60*24);
 
-let a = 1;
-let b = 2;
-alert(a + b);
+alert(60*24*30);
 
-let num = '123';
-let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
-alert(sum);
-
-let num3 = '123';
-alert(num3[0]);
-
-let a1 = 0;
-alert(++a1);
+alert(60*365);
