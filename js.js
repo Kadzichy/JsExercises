c2f1f2179d6cff929e224cf7bf3d49430752e56a
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:02:22 +0300

    Task 29

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
-let one = 'abcde';
-alert(one[one.length - 1]);
+let one = '12345'; 
+alert(Number(one[0]) + Number(one[1])+Number(one[2]) + Number(one[3])+Number(one[4]))
 
-let two = 'abcde';
-alert(two[two.length - 2]);
+let two = String(12345); 
+alert(Number(two[0]) + Number(two[1])+ Number(two[2]) + Number(two[3])+ Number(two[4]))
 
-let three = 'abcde';
-alert(three[three.length - 3]);
+let three = String(12345); 
+alert(three[0] * three[1] * three[2] * three[3]* three[4]);
+
+let four = '12345'; 
+alert(four[4] + four[3] + four[2] + four[1] + four[0]);
