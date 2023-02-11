996a07fe025b709f54f715182a740ca3d8d0a004
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:21:02 +0300

    Task 32

--- js.js
+++ js.js
@@ -1,24 +1,2 @@
-let num = 10;
-num++;
-num++;
-num--;
-alert(num);
-
-let num1 = 3;
-let num2 = num1++;
-alert(num1);
-alert(num2);
-
-let num3 = 3;
-let num4 = num3--;
-alert(num3);
-alert(num4);
-
-let num5 = 3;
-num5++;
-let num6 = num5--;
-alert(num5++);
-alert(--num6);
-
-let num7 = 3;
-alert(num7++);
+alert(0.1 * 0.2);
+alert(0.3 * 0.1);
