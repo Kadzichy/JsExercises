35dfdf8a91af7612f83347a903dd3b60d3b1c05d
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:19:04 +0300

    Task 31

--- js.js
+++ js.js
@@ -1,13 +1,24 @@
-let num = 1;
-num = num + 1;
-num = num * 1;
-num = num - 1;
-num = num / 1;
+let num = 10;
+num++;
+num++;
+num--;
 alert(num);
 
-let num1 = 47;
-num1 += 7;
-num1 -= 18;
-num1 *= 10;
-num1 /= 15;
+let num1 = 3;
+let num2 = num1++;
 alert(num1);
+alert(num2);
+
+let num3 = 3;
+let num4 = num3--;
+alert(num3);
+alert(num4);
+
+let num5 = 3;
+num5++;
+let num6 = num5--;
+alert(num5++);
+alert(--num6);
+
+let num7 = 3;
+alert(num7++);
