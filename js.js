4a2b1dbbe37c7e9f5cfb4b6ccb2902c38ab19658
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:37:31 +0300

    Task 37

--- js.js
+++ js.js
@@ -1,5 +1,20 @@
-alert(60*24);
+let r = 2, pi = 3.14;
+alert(pi * r ** 2);
 
-alert(60*24*30);
+let a = 15;
+s = a ** 2;
+alert(s);
 
-alert(60*365);
+let b = 1, c = 2;
+s = b + c;
+alert(s);
+
+let d = 1, f = 2;
+s = d * f;
+alert(s);
+
+let tc = 2;
+alert(tc * 33.8);
+
+let tf = 2;
+alert((tf - 32) / 1.8);
