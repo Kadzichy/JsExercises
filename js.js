1958a209d17e566f22ce19061e23428f4fa7a2a8
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 12:10:52 +0300

    Task 30

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
-let one = '12345'; 
-alert(Number(one[0]) + Number(one[1])+Number(one[2]) + Number(one[3])+Number(one[4]))
+let num = 1;
+num = num + 1;
+num = num * 1;
+num = num - 1;
+num = num / 1;
+alert(num);
 
-let two = String(12345); 
-alert(Number(two[0]) + Number(two[1])+ Number(two[2]) + Number(two[3])+ Number(two[4]))
-
-let three = String(12345); 
-alert(three[0] * three[1] * three[2] * three[3]* three[4]);
-
-let four = '12345'; 
-alert(four[4] + four[3] + four[2] + four[1] + four[0]);
+let num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /= 15;
+alert(num1);
