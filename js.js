3986e0bc3834f8ad407a4e5456791cce8fb4fe9a
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 13:04:07 +0300

    Task 41

--- js.js
+++ js.js
@@ -1,5 +1,18 @@
-let arr = [1, 2, 3];
-alert(arr.length);
+let arr = ['a', 'b', 'c'];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+alert(arr);
 
-let arr1 = [1, 2, 3, 1];
-alert(arr1[arr1.length - 1]) 
+let arr1 = [1, 2, 3, 4];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+arr1[3] += 3;
+alert(arr1)
+
+let arr2 = [1, 2, 3];
+arr2[0]++;
+arr2[1]++;
+arr2[2]++;
+alert(arr2);
