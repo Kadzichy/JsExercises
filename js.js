ea3859973bd220c73dcca7a6061d4e69c2d519e8
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-12 13:01:15 +0300

    Task 53

--- js.js
+++ js.js
@@ -1,13 +1,5 @@
-let obj1 = {
-	'1a': 1,
-	'b2': 2,
-	'с-с': 3,
-	'd 4': 4,
-	'e5': 5
-};
-
-console.log(obj1['1a']);
-console.log(obj1.b2);
-console.log(obj1['c-c']);
-console.log(obj1['d 4']);
-console.log(obj1.e5);
+let obj = {x: 1, y: 2, z: 3};
+obj['x'] = 1;
+obj['y'] = 4;
+obj['z'] = 9;
+console.log(obj);
