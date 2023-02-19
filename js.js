2a0f4974671e8ee7675c3bd7bf22866b5763cf69
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 12:36:50 +0300

    Task 66

--- js.js
+++ js.js
@@ -1,8 +1,2 @@
-console.log( typeof {x: 1, y: 2, z: 3} ); 
-console.log( typeof [1, 2, 3] );
-let arr = [1, 2, 3];
-console.log( typeof arr );
-let arr1 = [1, 2, 3];
-console.log( typeof arr[0] );
-let arr2 = ['1', '2', '3'];
-console.log( typeof arr[0] );
+console.log( Array.isArray([1, 2, 3]) );
+console.log( Array.isArray({x: 1, y: 2, z: 3}) ); 
