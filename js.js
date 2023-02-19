ecf21afcd9c830adb459209c5d8ac14431ad3494
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 12:42:19 +0300

    Task 69

--- js.js
+++ js.js
@@ -1,16 +1,11 @@
-let arr = [1, 2, 3];
-let arr1 = arr;
-arr1[0] = 'a';
-console.log(arr1)
+const arr = ['a', 'b', 'c'];
+arr[1] = '!';
+console.log(arr);
 
-let arr2 = [1, 2, 3];
-let arr3 = arr2;
-arr2[0] = 'a';
-arr3[1] = 'b';
+const arr1 = ['a', 'b', 'c'];
+arr1 = [1, 2, 3];
+console.log(arr1);
+
+const arr2 = ['a', 'b', 'c'];
+arr2 = ['a', 'b', 'c'];
 console.log(arr2);
-
-let arr4 = [1, 2, 3];
-let arr5 = arr4;
-arr4[0] = 'a';
-arr5[0] = 'b';
-console.log(arr5);
