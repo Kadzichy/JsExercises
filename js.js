a19c2dd0abc361e18f3ca50ce99724522a9786b9
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 12:35:18 +0300

    Task 64

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+console.log( typeof {x: 1, y: 2, z: 3} ); 
+console.log( typeof {} );
 let obj = {x: 1, y: 2, z: 3};
-delete obj.x;
-console.log('x' in obj);
+console.log( typeof obj );
+let obj1 = {x: 1, y: 2, z: 3};
+console.log( typeof obj1['x'] );
