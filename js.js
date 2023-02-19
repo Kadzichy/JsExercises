fb8a7599f894584de44ffe445b0ed414cd8c9e3f
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 13:41:36 +0300

    Task 82

--- js.js
+++ js.js
@@ -1,20 +1,28 @@
-let num = 3;
-if (num > 0 && num < 5) {
+let num1 = -10;
+let num2 = -10;
+if (num1 >= 0 || num2 >= 0) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-let num1 = 11;
-if (num1 >= 10 && num1 <=20) {
+let num1 = 0;
+let num2 = 0;
+if (num1 >= 0 || num2 >= 0) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-let num3 = -1;
-let num2 = 4;
-if (num2 >= 3 && num3 <=1) {
+let num = 2;
+if (num == 0 || num == 1 || num == 2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num = 1;
+if (num == 0 || num == 1) {
 	console.log('+++');
 } else {
 	console.log('---');
