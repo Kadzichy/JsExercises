a1b84b1cfc135ca37f34fb6c5a6b0489649693d3
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 13:32:42 +0300

    Task 76

--- js.js
+++ js.js
@@ -1,6 +1,15 @@
-let test = 10;
-if (test != 10) {
-	console.log('Переменная неравна 10'); 
+let test1 = 1;
+let test2 = 2;
+if (test2 > test1) {
+	console.log('test2 > test1');
 } else {
-	console.log('Переменная равна 10');
+	console.log('test2 < test1');
+}
+
+let test3 = 1;
+let test4 = 2;
+if (test3 == test4) {
+	console.log('test3 == test4');
+} else {
+	console.log('test3 != test4');
 }
