65b4575778596e2682c7df651e54bd37ecf56c44
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 13:43:13 +0300

    Task 83

--- js.js
+++ js.js
@@ -1,28 +1,5 @@
-let num1 = -10;
-let num2 = -10;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let num1 = 0;
-let num2 = 0;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let num = 2;
-if (num == 0 || num == 1 || num == 2) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let num = 1;
-if (num == 0 || num == 1) {
+let num = 1
+if (num == 0 || num > 1 && num < 5 ) {
 	console.log('+++');
 } else {
 	console.log('---');
