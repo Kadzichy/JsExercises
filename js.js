74c26a3a34067a21f9e3f624597a54b86ab9dc93
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 13:39:21 +0300

    Task 80

--- js.js
+++ js.js
@@ -1,15 +1,24 @@
 let test1 = '3';
-let test2 = '3';	
-if (test1 == test2) {
-	console.log('+');
+let test2 = '3';		
+if (test1 !== test2) {
+	console.log('+++');
 } else {
-	console.log('-');
+	console.log('---');
 }
 
-let test3 = 3;
-let test4 = '3';
-if (test3 === test4) {
-	console.log('+');
+let test2 = 3;
+let test3 = '3';		
+if (test2 !== test3) {
+	console.log('+++');
 } else {
-	console.log('-');
+	console.log('---');
+}
+
+let test4 = 3;
+let test5 = 2;
+		
+if (test4 !== test5) {
+	console.log('+++');
+} else {
+	console.log('---');
 }
