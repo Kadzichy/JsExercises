56b52fbe3a4b163caaa370d238da1dfc42620edc
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 13:29:40 +0300

    Task 73

--- js.js
+++ js.js
@@ -1,13 +1,6 @@
-let obj = {x: 1, y: 2, z: 3};
-console.log(obj.x);
-
-let obj1 = {x: 1, y: 2, z: 3};
-let key = 'x';
-console.log(obj1[key]);
-
-let obj2 = {x: 1, y: 2, z: 3};
-let sum = obj2.x + obj2.y + obj2.x;
-console.log(sum);
-
-let obj3 = {x: 1, y: 2, z: 3};
-console.log(Object.keys(obj3).length);
+let test = 9;
+if (test > 10) {
+	console.log('Переменная больше 10');
+} else {
+	console.log('Переменная не больше 10');
+}
