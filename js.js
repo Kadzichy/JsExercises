7f7699adc5e6e27e0ebd36d32dc4f66a25d53a15
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 12:39:04 +0300

    Task 67

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
-console.log( Array.isArray([1, 2, 3]) );
-console.log( Array.isArray({x: 1, y: 2, z: 3}) ); 
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1.x);
+let test2 = [1, 2, 3];
+console.log(test2);
+let test3 = [1, 2, 3];
+console.log(test3[1]);
+let test4 = [1, 2, 3];
+let test5 = 1;
+console.log(test4);
+let test6 = [1, 2, 3];
+let test7 = 1;
+console.log(test6[test7]);
