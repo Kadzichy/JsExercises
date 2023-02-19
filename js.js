649e568b9521f0f934efbb38e89334882d54d4a8
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-19 12:43:06 +0300

    Task 70

--- js.js
+++ js.js
@@ -1,11 +1,3 @@
-const arr = ['a', 'b', 'c'];
-arr[1] = '!';
-console.log(arr);
-
-const arr1 = ['a', 'b', 'c'];
-arr1 = [1, 2, 3];
-console.log(arr1);
-
-const arr2 = ['a', 'b', 'c'];
-arr2 = ['a', 'b', 'c'];
-console.log(arr2);
+const arr = [1, 2, 3, 4, 5];
+const res = arr[1] + arr[2];
+console.log(res);
