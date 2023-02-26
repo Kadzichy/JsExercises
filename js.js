42b78d8669856f1d759ab92e77724f308dd566cd
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-26 11:59:11 +0300

    Task 91

--- js.js
+++ js.js
@@ -1,69 +1,16 @@
-let test = 3;
-if (test) {
+let test1 = true;
+let test2 = true;
+if (!test1 && !test2) { 
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-let test1 = 'abc';
-if (test1) {
+let test3 = true;
+let test4 = true;
+let test5 = true;
+if (test3 || test4 && test5) { 
 	console.log('+++');
 } else {
 	console.log('---');
 }
-
-
-let test2 = '';
-if (test2) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test3 = 3 * 'abc';
-if (test3) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test4 = null;
-if (test4) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test5 = false;
-if (test5) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test6;
-if (test6) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test7 = 0;
-if (test7) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test8 = '0';
-if (test8) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test9 = -1;
-if (test9) {
-	console.log('+++');
-} else {
-	console.log('---');
