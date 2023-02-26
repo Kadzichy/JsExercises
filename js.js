fef9c6c20b6b579743cb3834f3a3a24ffa7bde12
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-26 11:54:23 +0300

    Task 88

--- js.js
+++ js.js
@@ -1,22 +1,5 @@
-let test = 1;
-		
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test1 = 1;
-		
-if (test1 != true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-
-let test2 = 3 * 'a';
-		
-if (test2 == true) {
+let test = true;
+if (test) {
 	console.log('+++');
 } else {
 	console.log('---');
