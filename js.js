52a2a70eff22e4a8fab6d49144f81dd23d48b0a5
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-26 11:57:38 +0300

    Task 90

--- js.js
+++ js.js
@@ -1,6 +1,69 @@
-let test1 = true;
-if (!test1) {
+let test = 3;
+if (test) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
+
+let test1 = 'abc';
+if (test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+
+let test2 = '';
+if (test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test3 = 3 * 'abc';
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test4 = null;
+if (test4) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test5 = false;
+if (test5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test6;
+if (test6) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test7 = 0;
+if (test7) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test8 = '0';
+if (test8) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test9 = -1;
+if (test9) {
+	console.log('+++');
+} else {
+	console.log('---');
