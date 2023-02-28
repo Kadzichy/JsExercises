c6c5cc7d552ae4f2283d5b4a90dab55208e9295e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-28 13:15:54 +0300

    Task 100

--- js.js
+++ js.js
@@ -1,11 +1,6 @@
-let a = 2 * (3 - 1);
-let b = 6 - 2;
-console.log(a == b)
-
-let a1 = 5 * (7 - 4);
-let b1 = 1 + 2 + 7;
-console.log(a1 == b1)
-
-let a2 = 2 ** 4;
-let b2 = 4 ** 2;
-console.log(a2 == b2)
+let age = confirm('Вам есть 18?');
+if (age) {
+	alert('Текст для взрослых');
+} else {
+	alert('Доступ запрещен');
+}
