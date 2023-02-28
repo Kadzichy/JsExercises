2ac87ffc327137c4a1ec620a2f374f28c4203aee
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-28 13:14:44 +0300

    Task 99

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
-let num = 1;
-let res = num >= 0 ? res = '1' : res = '2';
-console.log(res);
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+console.log(a == b)
+
+let a1 = 5 * (7 - 4);
+let b1 = 1 + 2 + 7;
+console.log(a1 == b1)
+
+let a2 = 2 ** 4;
+let b2 = 4 ** 2;
+console.log(a2 == b2)
