cf59b6cfd0af636bee71a9b1bc93a986e6933a53
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-01 20:28:42 +0300

    Task 109

--- js.js
+++ js.js
@@ -1,20 +1,20 @@
-let num = 12;
-if (String(num).lenght === 2) {
-	console.log('+++');
+let a = 123456;
+let b = String(n)[0];
+if (a == 1 || a == 2 || a == 3){
+    console.log('+++');
 } else {
 	console.log('---');
 }
 
-let num1 = 123;
-if (String(num1)[0] === 1) {
-	console.log('+++');
+let month = 12;
+if (month == 1 || month == 2 || month == 12){
+    console.log("Зима")
+} else if (month == 3 || month == 4 || month == 5){
+    console.log("Весна")
+} else if (month == 6 || month == 7 || month == 8){
+    console.log("Лето")
+} else if (month == 9 || month == 10 || month == 11){
+    console.log("Осень")
 } else {
-	console.log('---');
-}
-
-let num2 = 12;
-if (num2.length === 2) {
-	console.log('+++');
-} else {
-	console.log('---');
+    console.log("Неправильно")
 }
