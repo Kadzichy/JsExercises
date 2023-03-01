24e114f35e22550d8a386fae3d87a674f082a7a2
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-01 14:27:21 +0300

    Task 105

--- js.js
+++ js.js
@@ -1,4 +1,15 @@
-let arr = [1, 2, 3];
-if (arr.length == 3) {
-	console.log(arr[0] + arr[1] + arr[2]);
+let str = 'abcde';
+if (str[0] == 'a') {
+	console.log('true');
+}
+
+let str1 = 'abcde';
+let last = str1[str1.length - 1];
+if (last == 'e') {
+	console.log('true');
+}
+
+let str2 = 'abcde';
+if (str2[0] == 'a' || str2[0] == 'b') {
+	console.log('true');
 }
