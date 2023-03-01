03dbe619b786a7608e25d35060e510f27cab1bdf
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-01 20:34:20 +0300

    Task 112

--- js.js
+++ js.js
@@ -1,19 +1,4 @@
-for (let a = 1; a <= 100; a++) {
-	console.log(a);
-}
-
-for (let b = 11; b <= 33; b++) {
-	console.log(b);
-}
-
-for (let c = 0; c <= 100; c += 2) {
-	console.log(c);
-}
-
-for (let d = 1; d <= 99; d += 2) {
-	console.log(d);
-}
-
-for (let e = 100; e > 0; e--) {
-	console.log(e);
+let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let elem of arr) {
+	console.log(elem);
 }
