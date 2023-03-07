079c871c0ace9468e2372c110647889be74688fc
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-07 15:31:55 +0300

    Task 118

--- js.js
+++ js.js
@@ -1,23 +1,29 @@
+let arr = [2, 5, 9, 3, 1, 4];
 let res = 0;
-for (let i = 2; i <= 100; i++) {
-    if(i % 2 == 0){
-	res += i;
-    }
+for (let elem of arr) {
+	res += elem;
 }
-console.log(res);
+console.log(res)
 
+let arr1 = [2, 5, 9, 3, 1, 4];
 let res1 = 0;
-for (let i = 1; i <= 99; i++) {
-    if(i % 2 != 0){
-	res1 += i;
+for (let elem of arr1) {
+    if(elem % 2 == 0){
+	res1 += elem;
     }
 }
-console.log(res1);
+console.log(res1)
 
-let res2 = 1;
-for (let i = 1; i <= 20; i++) {
-    if(i % 2 != 0){
-	res2 *= i;
-    }
+let arr2 = [2, 5, 9, 3, 1, 4];
+let res2 = 0;
+for (let elem of arr2) {
+	res2 += elem**2;
 }
-console.log(res2);
+console.log(res2)
+
+let arr3 = [2, 5, 9, 3, 1, 4];
+let res3 = 1;
+for (let elem of arr3) {
+	res3 *= elem;
+}
+console.log(res3)
