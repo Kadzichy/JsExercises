3f5e5557db9117867cd24f0acfd96fb8d4580ad6
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-08 13:06:40 +0300

    Task 131

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ let arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
 let counter = 0;
 let counter1 = 0;
 for (let elem of arr) {
-	if (elem == 3) {
+	if (elem == 3) { 
 		counter++;
 	}
     if (elem == 2) {
