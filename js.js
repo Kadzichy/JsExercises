d8e3d55c32a49809fdcf693fb02cfd56d2bde27a
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-08 22:51:41 +0300

    Task 139

--- js.js
+++ js.js
@@ -1,8 +1,18 @@
-let arr = [
-	['a', 'b', 'c'],
-	['d', 'e', 'f'],
-	['g', 'h', 'i'],
-	['j', 'k', 'l'],
-];
-console.log(arr[3][2], arr[1][2], arr[2][0]); 
-console.log(arr[0][0]);
+let arr = [[1, 2, 3], [4, 5], [6]];
+let sum = 0;
+for (let mainArr of arr) {
+    for (let elem of mainArr) {
+        sum += elem
+    };
+};
+console.log(sum)
+
+let arr1 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]; 
+let sum1 = 0;
+for (let mainArr of arr1) {
+    for (let SubMainArr of mainArr) {
+        for (let SubSubMainArr of SubMainArr) {
+            sum1 += SubSubMainArr
+        };
+    };
+};
