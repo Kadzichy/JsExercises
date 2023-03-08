be0e2ebaa433102d8abab6df458163973ce34371
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-08 13:10:53 +0300

    Task 135

--- js.js
+++ js.js
@@ -1,10 +1,8 @@
-let arr = [10, 20, 30, 40, 21, 32, 51]; 
+let arr = {a: 10, b: 20, c: 30, d: 40, e: 50}; 
 let sum = 0;
 for (let elem of arr) {
-    let num = String(arr[i]);
-    let char = num[0];
-	if (char == 1 || char == 2) {
-		sum += elem;
+	if (+elem[0] === +elem[1] + 1) {
+		sum += +elem; 
 	}
 }
 
