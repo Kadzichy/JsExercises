6e9155b07702fecc940241ca3d6f0d15fb52aaf1
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-19 10:32:19 +0300

    Task 158

--- js.js
+++ js.js
@@ -1,7 +1,4 @@
-function sum(num1, num2, num3){
-    console.log(num1+num2+num3)
+function func(num1, num2, num3) {
+	console.log(num1 + num2 + num3);
 }
-let param1 = 1;
-let param2 = 2;
-let param3 = 3;
-sum(param1, param2, param3)
+func(11, 5, 56);
