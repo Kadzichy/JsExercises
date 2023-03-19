f918969b70c2751615cdd763faab7d20ce44dcad
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-19 10:43:21 +0300

    Task 168

--- js.js
+++ js.js
@@ -1,14 +1,15 @@
-let arr = [2, 4, 6];
-function func(ar1) {
-for (let elem of arr1) {
-    if (elem % 2 != 0) {
-        return false;
-    }
-};
-    return true;
-};
-if (func(arr)) {
-    console.log('Все числа в массиве четные');
-} else {
-    console.log('В массиве есть нечетные числа')
+function func(a, b) {
+	return a == b;
+}
+
+function func1(a, b) {
+	return a != b;
+}
+
+function func2(a, b) {
+	return a + b >= 10;
+}
+
+function func4(num) {
+	return num >= 0;
 }
