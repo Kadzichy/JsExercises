aa006d8b17f1518a18ad1c6ba286d19d558c8d2e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-19 10:50:15 +0300

    Task 171

--- js.js
+++ js.js
@@ -1,16 +1,23 @@
-function func1() {
-    return 3;
-}
-function func2() {
-    return 5;
-    }
-console.log(func1() + func2());
-
-function sum2(arr) {
-    let res = 0;
+let arr1 = [1, 2, 3, 4, 5,6]
+function sumArr(arr) {
+let res = 0;
 for (let elem of arr) {
     res += elem;
-}
-    return res;
-}
-console.log(sum2([1, 2, 3, 4, 5]));
+};
+return res;
+};
+console.log(sumArr(arr1));
+
+let num2 = 16;
+let newArr2 = [];
+function getDivider(number) {
+for (let i = 1; i <= number; i++) {
+    if (number % i == 0) {
+        newArr2.push(i)
+    } else {
+        continue;
+    };
+};
+    return newArr2;
+};
+console.log(getDivider(num2));
