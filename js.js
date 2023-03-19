baa45b80936a7fd05635daa82cabd8408edf5675
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-19 11:36:05 +0300

    Task 175

--- js.js
+++ js.js
@@ -4,10 +4,30 @@ function func() {
 }
 func();
 console.log(num);
-// 2
+
 let num1 = 1;
 function func1() {
-	let num1 = 2;
+	num1 = 2;
 }
-func1();
 console.log(num1);
+
+let num2 = 1;
+function func2() {
+	let num2 = 2;
+}
+func2();
+console.log(num2);
+
+let num3 = 1;
+function func3() {
+	let num3 = 2;
+}
+console.log(num3);
+func3();
+
+let num4 = 1;
+function func4() {
+	num4 = 2;
+}
+console.log(num4);
+func4();
