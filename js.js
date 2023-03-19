902595d593abdb1ffc629180f044a6aed43cf67c
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-19 10:18:30 +0300

    Task 152

--- js.js
+++ js.js
@@ -1,46 +1,26 @@
-console.log(Math.pow(2, 10));
+let str = 'js';
+console.log(str.toUpperCase());
 
-console.log(Math.sqrt(245));
+let str2 = 'JS';
+console.log(str2.toLowerCase());
 
-let arr = [4, 2, 5, 19, 13, 0, 10];
-let result = 0;
-for (let item of arr) {
-    result += (Math.pow(item, 3));
-}
-console.log(Math.sqrt(result));
+let str3 = 'я учу javascript!';
+let a1 = str3.substr(2, 3);
+let a2 = str3.substr(6, 10);
+console.log(a1, a2);
+let b1 = str3.substring(2, 5);
+let b2 = str3.substring(6, 16);
+console.log(b1, b2);
+let c1 = str3.slice(2, 5);
+let c2 = str3.slice(6, 16);
+console.log(c1, c2);
 
-let num = 245;
-let result1 = Math.round(Math.sqrt(num));
-console.log(result1);
-let result2 = Math.sqrt(num);
-console.log(result2.toPrecision(3));
-let result3 = Math.sqrt(num);
-console.log(result3.toPrecision(4));
+let str4 = 'abcde';
+console.log(str4.indexOf('c'));
 
-let num1 = 587;
-let obj = {
-        floor: null,
-        ceil: null,
-    }
-let result4 = Math.sqrt(num1)
-obj.floor = Math.floor(result4);
-let result5 = Math.sqrt(num1);
-obj.ceil = Math.ceil(result5);
-console.log(obj)
-
-console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
-console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandomInt(1, 100))
-
-let newArr = [];
-for (let i = 1; i <= 10; i++) {
-    newArr.push(getRandomInt(1, 1000));
+let str5 = 'abcde';
+if (str5.indexOf('a') >= 0) {
+        console.log('true')
+} else {
+        console.log('false')
 };
-console.log(newArr)
-
-let a = -1, b = -1;
-console.log(Math.abs(a - b))
