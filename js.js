69bc96504493796f57f2484bf0e667dcd72ad6dc
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-20 12:09:48 +0300

    Task 179

--- js.js
+++ js.js
@@ -1,23 +1,4 @@
-function func2(obj2) {
-	obj2 = '!';
+function func() {
+	return '!';
 }
-
-let obj2 = {a: 1, b: 2, c: 3};
-func(obj2.a);
-console.log(obj2);
-
-function func3(arr3) {
-	arr3.splice(1, 1);
-}
-
-let arr3 = [1, 2, 3];
-func(arr3);
-console.log(arr3);
-
-function func(arr) {
-	let newArr = arr;
-	newArr[0] = '!';
-}
-let arr = [1, 2, 3];
-func(arr);
-console.log(arr);
+console.log(func);
