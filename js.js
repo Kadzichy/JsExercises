f193dd541287e110e63c7e98683482b47f9a6461
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-20 12:07:11 +0300

    Task 176

--- js.js
+++ js.js
@@ -1,33 +1,17 @@
+function func(localNum) {
+	console.log(localNum);
+}
+func(1);
+
+function func(localNum) {
+	console.log(localNum);
+}
 let num = 1;
-function func() {
-	num = 2;
-}
-func();
-console.log(num);
+func(num);
 
-let num1 = 1;
-function func1() {
-	num1 = 2;
+function func(localNum) {
+	console.log(localNum);
 }
-console.log(num1);
-
-let num2 = 1;
-function func2() {
-	let num2 = 2;
-}
-func2();
-console.log(num2);
-
-let num3 = 1;
-function func3() {
-	let num3 = 2;
-}
-console.log(num3);
-func3();
-
-let num4 = 1;
-function func4() {
-	num4 = 2;
-}
-console.log(num4);
-func4();
+let num = 1;
+func(num);
+num = 2;
