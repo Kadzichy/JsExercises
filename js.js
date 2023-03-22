23354e8d27f09fcb653644f25083980fbc7a68a6
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:31:55 +0300

    Task 189

--- js.js
+++ js.js
@@ -1,46 +1,24 @@
-function test() {
-	let num = 1;
-	function func() {
-		console.log(num);
-	}
-	func();
+function func1() {
+	return function() {
+		return 1;
+	};
 }
-test();
+function func2() {
+	return function() {
+		return 2;
+	};
+}
+console.log(func1()() + func2()())
 
-function tes1() {
-	let num1 = 1;
-	
-	function func1() {
-		console.log(num1);
-	}
+function func() {
+	return function() {
+		return function() {
+            return function(){
+                return function(){
+			return '!';
+                };
+            };
+		};
+	};
 }
-test1();
-
-function test2() {
-	let num2 = 1;
-	function func2() {
-		console.log(num2);
-	}
-	func2();
-}
-
-function test3() {
-	let num3;
-	function func3() {
-		console.log(num3);
-	}
-	num3 = 1
-	func3();
-	
-	num3 = 2
-	func3();
-}
-test3();
-
-function test4(num1, num2) {
-	function func4() {
-		console.log(num1 + num2);
-	}
-	func4();
-}
-test4(1, 2);
+console.log(func()()()()())
