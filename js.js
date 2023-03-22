2b66e1dda07920944eca66864b362415ca5babd4
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:29:30 +0300

    Task 187

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
-let test = function func() {
-	console.log('!');
-	test(); // вызываем сами себя
-};
-
-test();
+function func(num1, num2) {
+         function square(num) {
+		return num * num;
+}
+function cube(num){
+        return num * num * num;
+    }
+	return square(num1) + cube(num2);
+}
+console.log(func(2, 3));
