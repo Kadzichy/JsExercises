7261f6d27436001bdbc59209c9dc566722bb72ea
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:39:00 +0300

    Task 195

--- js.js
+++ js.js
@@ -1,7 +1,16 @@
-function each(arr, callback) {
-let result = each([1, 2, 3, 4, 5], 
-	function(num) { 
-	return num * 2;
+let result = every([1, 2, 3, 4, 5], 
+	elem => elem > 0);
+
+let result1 = every([1, 2, 3, 4, 5], 
+	elem, index => elem * index > 10);
+
+let arr = [[1, 2, 3], [4, 5, 6], 
+	[7, 8, 9]]; 
+let result2 = each(arr, function(elem, 
+	index) { 
+	if (elem * index > 10) {
+		return true;
+	} else {
+		return false;
+	}
 });
-console.log(result);
-}
