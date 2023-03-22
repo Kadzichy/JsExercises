4f7724e85c9c2ac7d7a1cebde86e1a50effe7eea
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:26:05 +0300

    Task 184

--- js.js
+++ js.js
@@ -1,13 +1,7 @@
-let arr = [
-    function () { return 1 },
-    function () { return 2 },
-    function () { return 3 },
-];
-console.log(arr[2]());
-
-let sum = arr[0]() + arr[1]() + arr[2]();
-console.log(sum)
-
-for (let func of arr) {
-    console.log(func());
+let obj = {
+    func1: function () { return 1 },
+    func2: function () { return 2 },
+    func3: function () { return 3 },
 };
+let sum = obj.func1() + obj.func2() + obj.func3();
+console.log(sum);
