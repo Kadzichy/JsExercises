d39585386e75c7dc5fb3bf7b23d76162dddf23ff
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:30:25 +0300

    Task 188

--- js.js
+++ js.js
@@ -1,10 +1,46 @@
-function func(num1, num2) {
-         function square(num) {
-		return num * num;
+function test() {
+	let num = 1;
+	function func() {
+		console.log(num);
+	}
+	func();
 }
-function cube(num){
-        return num * num * num;
-    }
-	return square(num1) + cube(num2);
+test();
+
+function tes1() {
+	let num1 = 1;
+	
+	function func1() {
+		console.log(num1);
+	}
 }
-console.log(func(2, 3));
+test1();
+
+function test2() {
+	let num2 = 1;
+	function func2() {
+		console.log(num2);
+	}
+	func2();
+}
+
+function test3() {
+	let num3;
+	function func3() {
+		console.log(num3);
+	}
+	num3 = 1
+	func3();
+	
+	num3 = 2
+	func3();
+}
+test3();
+
+function test4(num1, num2) {
+	function func4() {
+		console.log(num1 + num2);
+	}
+	func4();
+}
+test4(1, 2);
