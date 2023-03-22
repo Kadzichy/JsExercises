f7c25b10752a14756bec4b19e1b4da8efd082978
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:27:22 +0300

    Task 185

--- js.js
+++ js.js
@@ -1,7 +1,43 @@
-let obj = {
-    func1: function () { return 1 },
-    func2: function () { return 2 },
-    func3: function () { return 3 },
-};
-let sum = obj.func1() + obj.func2() + obj.func3();
-console.log(sum);
+test(
+	function() {return 1;},
+	function() {return 2;},
+	function() {return 3;}
+);
+function test(func1, func2, func3) {
+	console.log( func1() + func2() + func3); 
+}
+
+function func1() {
+	return 1;
+}
+function func2() {
+	return 2;
+}
+function func3() {
+	return 3;
+}
+test(func1, func2, func3); 
+function test(func1, func2, func3) {
+	console.log( func1() + func2() + func3() );
+}
+
+let func1 = function() {
+	return 1;
+}
+let  func2 = function() {
+	return 2;
+}
+let func3 = function() {
+	return 3;
+}
+test(func1, func2, func3); 
+function test(func1, func2, func3) {
+	console.log( func1() + func2() + func3() );
+}
+
+test(function(num) {
+	return num ** 3;
+});
+function func() {
+	console.log(func(3));
+}
