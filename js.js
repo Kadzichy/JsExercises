edf1ceda8407a053f75a1bc841447926423761f2
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:28:21 +0300

    Task 186

--- js.js
+++ js.js
@@ -1,43 +1,6 @@
-test(
-	function() {return 1;},
-	function() {return 2;},
-	function() {return 3;}
-);
-function test(func1, func2, func3) {
-	console.log( func1() + func2() + func3); 
-}
+let test = function func() {
+	console.log('!');
+	test(); // вызываем сами себя
+};
 
-function func1() {
-	return 1;
-}
-function func2() {
-	return 2;
-}
-function func3() {
-	return 3;
-}
-test(func1, func2, func3); 
-function test(func1, func2, func3) {
-	console.log( func1() + func2() + func3() );
-}
-
-let func1 = function() {
-	return 1;
-}
-let  func2 = function() {
-	return 2;
-}
-let func3 = function() {
-	return 3;
-}
-test(func1, func2, func3); 
-function test(func1, func2, func3) {
-	console.log( func1() + func2() + func3() );
-}
-
-test(function(num) {
-	return num ** 3;
-});
-function func() {
-	console.log(func(3));
-}
+test();
