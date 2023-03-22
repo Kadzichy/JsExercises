60cdad209603021893066da39b5e07864c818670
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-22 18:20:23 +0300

    Task 181

--- js.js
+++ js.js
@@ -1,14 +1,13 @@
 function func() {
-	return 'alec';
+	console.log('!');
 }
-console.log(func());
-console.log(func);
 
-func = 123; // затрем переменную func
-console.log(func); 
-
-function func1() {
-	return 3;
+let func1 = function() {
+	console.log('!');
 }
-let func2 = func1;
-console.log(func1() + func2());
+
+func2(); 
+
+function func2() {
+	console.log('alec');
+}
