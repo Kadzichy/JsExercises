c916aa68371339e26fec0fbe3c426a6d8275a81c
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-29 17:04:46 +0300

    Task 199

--- js.js
+++ js.js
@@ -1,17 +1,6 @@
-let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; 
-
-let result = arr.map(function(elem) {
-	return elem.map(function(num) {
-		return num * num;
-	});
+let arr = [1, 2, 3, 4];
+let sum = 0;
+arr.forEach(function(elem) {
+	sum += elem**2;
 });
-
-console.log(result);
-
-let arr1 = ['a', 'b', 'c', 'd', 'e'];
-
-let result1 = arr1.map(function(elem, index) {
-	return elem + index;
-});
-
-console.log(result1); 
+console.log(sum);
