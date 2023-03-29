76dff2dcf8b0e468424c19bd2779e7e08eacc2c0
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-29 17:06:41 +0300

    Task 200

--- js.js
+++ js.js
@@ -1,6 +1,21 @@
-let arr = [1, 2, 3, 4];
-let sum = 0;
-arr.forEach(function(elem) {
-	sum += elem**2;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result = arr.filter((elem, index) => elem * index <= 30);
+console.log(result);
+
+const arr1 = [1, 2, [3, 4], 5, [6, 7]];
+const newarr1 = [];
+let result1 = arr1.filter(function (elem) {
+    if (typeof elem !== "object") {
+        newarr6.push(elem);
+    }
 });
-console.log(sum);
+console.log(newarr1);
+
+const arr2 = [-1, 2, -3, -25, 30];
+let count = 0;
+let result2 = arr2.filter((elem) => {
+    if (elem < 0) {
+        count++;
+    }
+});
+console.log(count);
