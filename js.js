3d37146e8ad7e8a3551cc9fcf9f048948b92b519
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-03-29 17:02:43 +0300

    Task 197

--- js.js
+++ js.js
@@ -1,23 +1,13 @@
-let arr = [1, 2, 3, 4, 5];
 function func(arr) {
-	console.log(arr.shift()); 
-	console.log(arr); 
-	
-	console.log(arr.shift()); 
-	console.log(arr); 
-	
-	console.log(arr.shift()); 
-	console.log(arr); 
-}
-
-let arr1 = [1, 2, 3, 4, 5];
-function getSum(arr1) {
-	let sum = arr1.shift();
-	
-	if (arr1.length !== 0) {
-		sum += getSum(arr1);
+	for (let i = 0; i < arr.length; i++) {
+		if (typeof arr[i] == 'object') {
+			arr[i] = func(arr[i]);
+		} else {
+			arr[i] = arr[i] * arr[i];
+		}
 	}
 	
-	return sum;
+	return arr;
 }
-console.log(getSum());
+
+console.log(func([1, [2, 7, 8], [3, 4, [5, 6]]]));
