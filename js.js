8b638bbfe5bfa16d2060f6dee852a5f172100a2e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-03 19:17:24 +0300

    Task 201

--- js.js
+++ js.js
@@ -1,21 +1,7 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let result = arr.filter((elem, index) => elem * index <= 30);
-console.log(result);
+let arr1 = [2, 4, 6, 8, 10];
+let result1 = arr1.every((elem) => elem > 0);
+console.log(result1);
 
-const arr1 = [1, 2, [3, 4], 5, [6, 7]];
-const newarr1 = [];
-let result1 = arr1.filter(function (elem) {
-    if (typeof elem !== "object") {
-        newarr6.push(elem);
-    }
-});
-console.log(newarr1);
-
-const arr2 = [-1, 2, -3, -25, 30];
-let count = 0;
-let result2 = arr2.filter((elem) => {
-    if (elem < 0) {
-        count++;
-    }
-});
-console.log(count);
+let arr2 = [2, 4, 6, 8, 10];
+let result2 = arr2.every((elem, index) => elem * index < 30);
+console.log(result2);
