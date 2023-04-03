bc227163573fe549eea52a3ca0c307f80d958c56
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-03 19:24:44 +0300

    Task 203

--- js.js
+++ js.js
@@ -1,7 +1,15 @@
-let arr = [2, 4, 6, 8, 10];
-let result = arr.some(elem => elem > 0); 
-console.log(result);
+let arr = [1, 2, 3, 4, 5];
+function func(num1, num2, num3, 
+	num4, num5) { 
+return num1 + num2 + num3 + num4 + num5; 
+}
+console.log(func(...arr));
 
-let arr2 = [2, 4, 6, 8, 10];
-let result2 = arr2.some((elem, index) => elem * index < 30);
-console.log(result2);
+function func2(n1, n2, n3, n4, n5, n6, n7, n8) {
+return (n1 + n2 + n3 + n4) * (n5 + n6 + n7 + n8);
+}
+console.log(func2(1, [2, 3, 4], 5, [6], [7, 8]));
+
+let arr3 = [1, 2, 3, 4, 5];
+let min = Math.min(...arr3);
+console.log(min)
