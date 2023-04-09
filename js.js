6c63ddd201e453732d22a5442c97bd3ee30618ac
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:21:28 +0300

    Task 234

--- js.js
+++ js.js
@@ -1,8 +1,13 @@
-function isLeap(year){
-    year = new Date(2015, 5, 10);
-    if (year.getDate() == 29) {
-	return true
-} else {
-	return false
+function checkDate(year, month, day) {
+    if (
+        new Date(year, month, day).getFullYear() == year &&
+        new Date(year, month, day).getMonth() == month &&
+        new Date(year, month, day).getDate() == day
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
-}
+console.log(checkDate(2025, 0, 31));
+console.log(checkDate(2025, 0, 32));
