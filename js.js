4579097c7435449d3ea30deec0b3d6927c0a075f
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:07:43 +0300

    Task 217

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ let options = {
     width: 400,
     height: 500,
 };
-let {color, width, height} = options;
-console.log(color)
-console.log(width)
-console.log(height)
+let {color: c, width: w, height: h} = options;
+console.log(c)
+console.log(w)
+console.log(h)
