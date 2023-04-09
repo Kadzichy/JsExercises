9e6c3a3963e08b68919d88b9cca6af5533d81ac2
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:11:49 +0300

    Task 223

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
 let date = new Date();
-console.log(date.getDate());     
-
-console.log(date.getMonth());    
-
-console.log(date.getFullYear()); 
+function addZero(num) {
+    if (num >= 0 && num <= 9) {
+        return "0" + num;
+    } else {
+        return num;
+    }
+}
+console.log(
+    addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' +
+    addZero(date.getDate()) + '.' + addZero(date.getMonth() + 1) + '.' + date.getFullYear());
