b9b484b57433b5b44206d37fb7b909501ce27a83
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:12:32 +0300

    Task 224

--- js.js
+++ js.js
@@ -1,11 +1,3 @@
-let date = new Date();
-function addZero(num) {
-    if (num >= 0 && num <= 9) {
-        return "0" + num;
-    } else {
-        return num;
-    }
-}
-console.log(
-    addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' +
-    addZero(date.getDate()) + '.' + addZero(date.getMonth() + 1) + '.' + date.getFullYear());
+let str = '2025-12-31';
+let res = str.split('-').reverse().join('.');
+console.log(res);
