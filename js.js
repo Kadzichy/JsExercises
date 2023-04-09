d4c60de5e7497286d69a7cc05254d976bff2e1e9
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:02:46 +0300

    Task 211

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
-let arr = ['John', 'Smit', 'development', 
-	'programmer', 2000]; 
-let [, , department, position, ] = arr;
-console.log(department)
-console.log(position)
+let arr = [2025, 12, 31, 23, 59, 59];
+let [year, month, day] = arr;
+
+console.log(year);  
+console.log(month); 
+console.log(day);   
