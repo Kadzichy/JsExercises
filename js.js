296b45c03fa4e9c91f2913bdbdff23a9418d49c6
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-09 16:22:40 +0300

    Task 236

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
-let now = new Date();
-let year = now.getFullYear();
-for (let month = 0; month <= 11; month++) {
-let date = new Date(year, month, 1);
-if (date.getDay() == 0) {
-    console.log(year + "-" + month + "-1");
-}
-}
+let day = new Date(2023, 4, 2, 23);
+let now = new Date(2023, 4, 3, 15);
+let diff1 = now - day;
+console.log(diff1 / 1000);
+
+let day1 = new Date(2023, 4, 3, 23);
+let now1 = new Date(2023, 4, 3, 15);
+let diff2 = day1 - now1;
+console.log(diff2 / 1000);
