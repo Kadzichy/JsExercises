31496993bb020a844249f01dbabd251d36d3ae40
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-16 10:18:58 +0300

    Task 242

--- js.js
+++ js.js
@@ -1,15 +1,12 @@
-let p1 = document.querySelector("#elem1");
-let p2 = document.querySelector("#elem2");
-let p3 = document.querySelector("#elem3");
-console.log(p1);
-console.log(p2);
-console.log(p3);
-
-let pOne = document.querySelector("#block p");
-console.log(pOne);
-
-let p1One = document.querySelector(".block p");
-console.log(p1One);
-
-let www = document.querySelector(".www");
-console.log(www);
+button1 = document.querySelector("#button1");
+button2 = document.querySelector("#button2");
+button3 = document.querySelector("#button3");
+button1.addEventListener("click", function () {
+    document.write(1)
+});
+button2.addEventListener("click", function () {
+    document.write(2)
+});
+button3.addEventListener("click", function () {
+    document.write(3)
+});
