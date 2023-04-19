92905679887089c55df34462669a8276e44e7516
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-19 20:43:07 +0300

    Task 279

--- js.js
+++ js.js
@@ -1,10 +1,5 @@
-let radios = document.querySelectorAll('input[type="radio"]');
-let button = document.querySelector("#button");
-let p = document.querySelector("p");
-button.addEventListener("click", function () {
-    for (let radio of radios) {
-        if (radio.checked) {
-            p.textContent = radio.value;
-        }
-    }
+let elem = document.querySelector('#elem');
+let p = document.querySelector('p')
+elem.addEventListener('change', function() {
+    p.textContent = this.value
 });
