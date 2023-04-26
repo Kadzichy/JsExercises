37a977ec2512ac74f87cfe937bbab3bd2bfc725d
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-26 20:51:51 +0300

    Task 296

--- js.js
+++ js.js
@@ -1,19 +1,11 @@
-window.onload = function() {
-    const container = document.querySelector('#container');
-const first = document.querySelector('#first');
-const second = document.querySelector('#second');
-const button = document.querySelector('#button');
-
-container.addEventListener('click', (event) => {
-  console.log(`Click on ${event.target.tagName} in container`);
-});
-first.addEventListener('click', (event) => {
-  console.log(`Click on ${event.target.tagName} in first`);
-});
-second.addEventListener('click', (event) => {
-  console.log(`Click on ${event.target.tagName} in second`);
-});
-button.addEventListener('click', (event) => {
-  console.log(`Click on ${event.target.tagName} in button`);
+let container = document.querySelector('#container');
+container.addEventListener('click', function(event) {
+	if (event.target.tagName === 'LI') {
+		event.target.innerHTML += '!';
+	} else if (event.target.tagName === 'UL') {
+		let newLi = document.createElement('li');
+		newLi.innerHTML = 'New item';
+		event.target.appendChild(newLi);
+	}
 });
 }
