8706c705ea31c25251ee6d2704527fb9661368cb
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-26 18:53:33 +0300

    Task 282

--- js.js
+++ js.js
@@ -1,12 +1,7 @@
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-input1.addEventListener('keyup', function() {
-  if (this.value.length >= 2) {
-    input2.focus();
-  }
-});
-input2.addEventListener('keyup', function() {
-  if (this.value.length >= 2) {
-    this.blur();
-  }
-});
+window.onload = function() {
+const select = document.getElementById("mySelect");
+const paragraph = document.getElementById("myParagraph");
+const button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+  const selectedOption = select.options[select.selectedIndex].text;
+  paragraph.textContent = selectedOption;
