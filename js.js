421367dac06248de5de850cf5b82ae39e1561f5d
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-26 20:17:05 +0300

    Task 286

--- js.js
+++ js.js
@@ -1,7 +1,14 @@
-window.onload = function() {
-const mySelect = document.getElementById("mySelect");
-const options = mySelect.options;
-for (let i = 0; i < options.length; i++) {
-  options[i].text += " " + options[i].value;
-}
+const select1 = document.getElementById('mySelect');
+const button = document.getElementById('btn');
+button.addEventListener('click', function() {
+  const selectedOption = select1.options[select1.selectedIndex];
+  alert(selectedOption.textContent);
+});
+
+let select2 = document.getElementById('mySelect');
+let button1 = document.getElementById('myButton');
+button1.addEventListener('click', function() {
+  let selectedOption = select2.options[select2.selectedIndex];
+  selectedOption.textContent += '!';
+});
 }
