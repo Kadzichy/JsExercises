cffe56ec2b80127e60c4950498afee2acb55fda6
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-26 18:56:01 +0300

    Task 284

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 window.onload = function() {
-const today = new Date();
-const currentMonth = today.getMonth() + 1;
-const monthsSelect = document.getElementById("months");
-const currentMonthOption = monthsSelect.querySelector(`option[value="${currentMonth}"]`);
-currentMonthOption.selected = true;
-}
+const input = document.getElementById('input');
+const select = document.getElementById('select');
+input.addEventListener('blur', () => {
+  const value = parseInt(input.value);
+  if (!isNaN(value)) {
+    select.selectedIndex = value - 1;
+  }
+});
