364e9844e782e28f6d49b48fd0b829c43e67bc33
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-04-26 20:33:23 +0300

    Task 290

--- js.js
+++ js.js
@@ -1,7 +1,12 @@
 window.onload = function() {
-let elem = document.getElementById('elem');
-document.addEventListener('mousemove', function(event) {
-	elem.innerHTML = event.clientX + ' : ' + 
-		event.clientY; 
-});
+let elem = document.querySelector('#elem');
+elem.addEventListener('click', func);
+elem.addEventListener('dblclick', func);
+function func(event) {
+  if (event.type === 'click') {
+    event.target.style.backgroundColor = 'green';
+  } else if (event.type === 'dblclick') {
+    event.target.style.backgroundColor = 'red';
+  }
+}
 }
