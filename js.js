90d53df36f4f7d1dbf4dab4fe47b32476f8af6a9
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-01 16:36:52 +0300

    Task 308
    Task 308

--- js.js
+++ js.js
@@ -1,10 +1,3 @@
-let elem = document.getElementById('elem');
-function func(name, surname) {
-    console.log(this.value + ', ' + name 
-        + ' ' + surname); 
-}
-
-let funcBound = func.bind(elem);
-
-funcBound('John', 'Smit'); // выводит 'привет, John Smit'
-funcBound('Eric', 'Luis');
+setInterval(function() {
+	console.log('Oleg');
+}, 3000);
