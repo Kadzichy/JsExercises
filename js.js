dd8c08e9016c8fda368a40d588b31eb4c894f3f2
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-01 17:02:48 +0300

    Task 323
    Task 323

--- js.js
+++ js.js
@@ -1,16 +1,11 @@
 window.onload = function() {
-let elems = document.querySelectorAll('li');
-for (let elem of elems) {
-	elem.addEventListener('click', function() {
-		elem.remove();
-	});
-}
+let elem = document.getElementById('elem');
+    elem.insertAdjacentHTML('afterbegin', '<li>start</li>');
+    elem.insertAdjacentHTML('beforeend', '<li>finish</li>');
 
-document.getElementById('button').addEventListener('click', function() {
-  var parent = document.getElementById('parent');
-  var lastChild = parent.lastElementChild;
-  if (lastChild) {
-    parent.removeChild(lastChild);
-  }
-});
+var parent = document.getElementById('parent');
+var newElem = document.createElement('li');
+newElem.textContent = 'new';
+parent.insertBefore(newElem, document.getElementById('elem1'));
+
 }
