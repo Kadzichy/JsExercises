f8fca7a4f8e1797c31da7bfdc03c92d8a630a058
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-01 16:41:39 +0300

    Task 312
    Task 312

--- js.js
+++ js.js
@@ -1,28 +1,39 @@
-function startCountdown() {
-        let count = 100;
-        let countdown = setInterval(function() {
-          console.log(count);
-          count--;
-          if (count === 0) {
-            clearInterval(countdown);
-          }
-        }, 1000);
-      }
-
+const startButton = document.querySelector('#start-button');
+const stopButton = document.querySelector('#stop-button');
+const output = document.querySelector('#output');
+let count = 100;
 let timerId;
-const countdownBtn = document.getElementById("countdownBtn");
-function startCountdown() {
-  let counter = 100;
-  timerId = setInterval(() => {
-    console.log(counter);
-    counter--;
-    if (counter < 0) {
-      clearInterval(timerId);
-    }
-  }, 1000);
-}
-countdownBtn.addEventListener("click", () => {
-  if (!timerId) {
-    startCountdown();
+function countdown() {
+  count--;
+  output.textContent = count;
+  if (count === 0) {
+    clearInterval(timerId);
   }
+}
+startButton.addEventListener('click', function() {
+  timerId = setInterval(countdown, 1000);
+});
+stopButton.addEventListener('click', function() {
+  if (timerId) {
+    clearInterval(timerId);
+  }
+});
+setInterval(function() {
+  if (count === 0) {
+    clearInterval(timerId);
+  }
+}, 1000);
+
+let start = document.querySelector('#start');
+let stop = document.querySelector('#stop');
+let timerId1;
+start.addEventListener('click', function() {
+timerId1 = setInterval(function() {
+let date = new Date;
+console.log(date.getMinutes() + ' ' +
+date.getSeconds());
+}, 1000);
+});
+stop.addEventListener('click', function() {
+clearInterval(timerId);
 });
