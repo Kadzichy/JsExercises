f880b48aa04b7cb866562c535ee1c3b8b5da38bb
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-01 16:44:39 +0300

    Task 314
    Task 314

--- js.js
+++ js.js
@@ -1,11 +1,15 @@
 window.onload = function() {
-let countdownInput = document.getElementById('elem');
-  let countdownValue = parseInt(countdownInput.value);
-  let timerId = setInterval(function() {
-    countdownValue--;
-    countdownInput.value = countdownValue;
-    if (countdownValue === 0) {
-      clearInterval(timerId);
-    }
+let elem = document.querySelector('#elem');
+elem.addEventListener('click', function() {
+  setInterval(function() {
+    elem.value = Number(elem.value) + 1;
   }, 1000);
+});
 }
+
+let elem = document.querySelector('#elem');
+elem.addEventListener('click', () => {
+  setInterval(() => {
+    elem.value = Number(elem.value) + 1;
+  }, 1000);
+});
