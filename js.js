369d9e0945f3f221b7b906f43cba8d6b882931d4
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-02 16:42:48 +0300

    Task 339
    Task 339

--- js.js
+++ js.js
@@ -1,19 +1,14 @@
 window.onload = function() {
 let elem = document.querySelector('#elem');
-let input = document.querySelector('#input');
-input.value = elem.textContent; 
-input.addEventListener('input', function() {
-    elem.textContent = this.value;
-});
-
-let elem2 = document.querySelector('#elem1');
-elem2.addEventListener('click', function() {
-	let input = document.createElement('input');
-	input.value = elem2.textContent;
-	input.addEventListener('blur', function() {
-		elem2.textContent = this.value;
-		this.remove(); // удалим инпут
+elem.addEventListener('click', function func()	 { 
+let input = document.createElement('input');
+input.value = elem.textContent;	
+elem.textContent = '';
+elem.appendChild(input);	
+input.addEventListener('blur', function() {
+elem.textContent = this.value;
+elem.addEventListener('click', func); 
 	});
-	elem2.parentElement.appendChild(input);
+elem.removeEventListener('click', func);
 });
 }
