a25b86a679688ab391202fe9a4c6b8a6f26e5fc6
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-02 17:03:37 +0300

    Task 348
    Task 348

--- js.js
+++ js.js
@@ -1,21 +1,7 @@
 window.onload = function() {
-const array = ['item 1', 'item 2', 'item 3'];
-  const list = document.getElementById('myList');
-  for (let i = 0; i < array.length; i++) {
-    const listItem = document.createElement('li');
-    const text = document.createTextNode(array[i]);
-    listItem.appendChild(text);
-    list.appendChild(listItem);
-  }
-
-let employees = [
-    {name: 'employee1', age: 30, salary: 400}, 
-    {name: 'employee2', age: 31, salary: 500}, 
-    {name: 'employee3', age: 32, salary: 600}, 
-];
-let table = '<table><thead><tr><th>Name</th><th>Age</th><th>Salary</th></tr></thead><tbody>';
-for (let i = 0; i < employees.length; i++) {
-    table += '<tr><td>' + employees[i].name + '</td><td>' + employees[i].age + '</td><td>' + employees[i].salary + '</td></tr>';
+function setAttr(selector, attrName, attrValue) {
+  const elem = document.querySelector(selector);
+  elem.setAttribute(attrName, attrValue);
 }
-table += '</tbody></table>';
-document.body.innerHTML = table;
+setAttr('#myButton', 'disabled', true);
+}
