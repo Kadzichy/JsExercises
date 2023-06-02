4937fd8a2a6fc0fde2934b5d0ea7977f1efa32a9
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-02 16:26:36 +0300

    Task 327
    Task 327

--- js.js
+++ js.js
@@ -1,23 +1,11 @@
-window.onload = function() {
-var elem = document.querySelector('#elem'); 
-if (elem.matches('.www')) { 
-  console.log('Элемент с классом "www" найден');
-} else {
-  console.log('Элемент не содержит класс "www"');
-}
-
-var elem = document.querySelector('#elem'); 
-if (elem.matches('p')) { 
-  console.log('Элемент является абзацем');
-} else {
-  console.log('Элемент не является абзацем');
-}
-
-let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
-if (elem1.contains(elem2)) {
-  console.log('Элемент elem2 является потомком элемента elem1');
-} else {
-  console.log('Элемент elem2 не является потомком элемента elem1');
-}
+let parent = document.querySelector('#parent');
+let arr = [1, 2, 3, 4, 5];
+for (let elem of arr) {
+  let p = document.createElement('p');
+  p.textContent = elem;
+  parent.appendChild(p);
+  p.addEventListener('click', function() {
+    let currentValue = parseInt(p.textContent);
+    p.textContent = currentValue + 1;
+  });
 }
