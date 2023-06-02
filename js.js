602f8af7baff5395aab7e2f8e9b3e4913801422d
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-02 16:25:10 +0300

    Task 326
    Task 326

--- js.js
+++ js.js
@@ -1,7 +1,23 @@
 window.onload = function() {
-function cloneInput() {
-  var input = document.getElementById("input-field"); 
-  var clonedInput = input.cloneNode(true); 
-  input.parentNode.insertBefore(clonedInput, input.nextSibling); 
+var elem = document.querySelector('#elem'); 
+if (elem.matches('.www')) { 
+  console.log('Элемент с классом "www" найден');
+} else {
+  console.log('Элемент не содержит класс "www"');
+}
+
+var elem = document.querySelector('#elem'); 
+if (elem.matches('p')) { 
+  console.log('Элемент является абзацем');
+} else {
+  console.log('Элемент не является абзацем');
+}
+
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+if (elem1.contains(elem2)) {
+  console.log('Элемент elem2 является потомком элемента elem1');
+} else {
+  console.log('Элемент elem2 не является потомком элемента elem1');
 }
 }
