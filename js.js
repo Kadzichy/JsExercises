9d0911474dae6c67a42beb430ff2bfe7bd8d0d90
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-02 16:51:31 +0300

    Task 344
    Task 344

--- js.js
+++ js.js
@@ -1,15 +1,24 @@
 window.onload = function() {
-let elem = document.querySelector('#elem');
-let toggle = document.querySelector('#toggle');
-let isHidden = false;
-toggle.addEventListener('click', function() {
-  if (isHidden) {
-    elem.classList.remove('hidden');
-    toggle.value = 'hide';
-  } else {
-    elem.classList.add('hidden');
-    toggle.value = 'show';
-  }
-  isHidden = !isHidden;
-});
+let buttons = document.querySelectorAll('button');
+for (let button of buttons) {
+	button.addEventListener('click', function() {
+		let elem = document.querySelector('#' + this.dataset.elem);
+		elem.classList.toggle('hidden');
+	});
+}
+
+let buttons1 = document.querySelectorAll('button');
+let elems   = document.querySelectorAll('p');
+for (let i = 0; i < buttons1.length; 
+	i++) { 
+	buttons1[i].addEventListener('click', function() {
+		elems[i].classList.toggle('hidden');
+	});
+}
+let buttons2 = document.querySelectorAll('button');
+for (let button of buttons2) {
+	button.addEventListener('click', function() {
+		this.previousElementSibling.classList.toggle('hidden');
+	});
+}
 }
