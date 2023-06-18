9ccc847a792e73fb9c93964c990c04e5a9afcf6b
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-06-18 11:07:45 +0300

    Task 352
    Task 352

--- js.js
+++ js.js
@@ -1,7 +1,13 @@
 window.onload = function() {
+function appendText(elem, text) {
+  elem.textContent += text;
+}
 let paragraphs = document.querySelectorAll('p');
-paragraphs.forEach(function(paragraph, index) {
-  let paragraphNumber = index + 1;
-  paragraph.textContent = paragraphNumber + '. ' + paragraph.textContent;
+paragraphs.forEach(function(paragraph) {
+  appendText(paragraph, '!');
 });
+
+function setValue(input, text) {
+  input.value = text;
+}
 }
